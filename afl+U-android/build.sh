f7d32bb9fa4cee41448bd20c75c61d5c654486d0
Nicholas1126 <282235411@qq.com>
2021-03-13 16:34:25 +0800

    Fix some bugs on PLE-703L.

--- afl+U-android/build.sh
+++ afl+U-android/build.sh
@@ -1,1 +1,6 @@
 ndk-build -B NDK_PROJECT_PATH=. APP_PLATFORM=android-22
+
+# cd libs/arm64-v8a/
+# adb push afl-fuzz /data/local/tmp
+# adb push libandroid-ashmem.so /data/local/tmp
+# cd -
--- tests/test-AFL-android/fuzz.sh
+++ tests/test-AFL-android/fuzz.sh
@@ -4,8 +4,16 @@ stty rows 160 cols 160
 cd /data/local/tmp/python3.9
 . ./env.sh
 cd ..
+chmod 777 afl-fuzz 
+chmod 777 afl_test_local
+chmod 777 libandroid-ashmem.so
+chmod 777 fuzz.sh
+chmod 777 frida-afl-fuzzer
+mkdir in out
+echo "helloworld!" > in/seed
 cd /sys/devices/system/cpu
 echo performance | tee cpu*/cpufreq/scaling_governor
 cd -
 export LD_PRELOAD=/data/local/tmp/libandroid-ashmem.so
+export LD_LIBRARY_PATH=/data/local/tmp:$LD_LIBRARY_PATH
 ./afl-fuzz -U -i ./in -o ./out -m none -- /data/local/tmp/python3.9/usr/bin/python3.9 ./frida-afl-fuzzer -spawn /data/local/tmp/afl_test_local
